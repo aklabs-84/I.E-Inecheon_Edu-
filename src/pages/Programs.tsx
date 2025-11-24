231e3e12bfff33133aa8cf1ea710c97b4e4e0ccc
김예령 <vpdrnls3208@gmail.com>
2025-11-24 12:25:18 +0900

    필터수정하기

--- src/pages/Programs.tsx
+++ src/pages/Programs.tsx
@@ -242,11 +242,12 @@ const Programs = () => {
               />
             </div>
             
-            {/* Filter Button */}
+            {/* Filter Button 
             <Button variant="outline" size="default" className="flex items-center gap-2">
               <Filter className="h-4 w-4" />
               필터
             </Button>
+            */}
           </div>
 
           {/* Filters */}
